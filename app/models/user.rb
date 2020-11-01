78d73bc43e327ffe3e8aabab8c33be2f456d27fa
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2020-11-01 16:11:02 +0900

    レビュー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
 
-    zenkaku_hira_kana_kanji= with: /\A[ぁ-んァ-ン一-龥]/
+    zenkaku_hira_kana_kanji = /\A[ぁ-んァ-ン一-龥]/
+    zenkaku_kana = /\A[ァ-ヶー－]+\z/
 
     validates :nickname
-    validates :password,format: { with:/\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i  }
-    validates :first_name, format: { zenkaku_hira_kana_kanji, message: 'は全角ひらがな、全角カタカナ、漢字で入力して下さい。'}
-    validates :last_name, format: { zenkaku_hira_kana_kanji, message: 'は全角ひらがな、全角カタカナ、漢字で入力して下さい。'}
-    validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。'}
-    validates :last_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい。'}
+    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i  }
+     validates :first_name, format: {with: zenkaku_hira_kana_kanji , message: 'は全角ひらがな、全角カタカナ、漢字で入力して下さい。'}
+     validates :last_name, format: {with: zenkaku_hira_kana_kanji , message: 'は全角ひらがな、全角カタカナ、漢字で入力して下さい。'}
+    validates :first_kana, format: {with: zenkaku_kana , message: 'は全角カタカナで入力して下さい。'}
+    validates :last_kana, format: {with: zenkaku_kana , message: '全角カタカナで入力して下さい。'}
     validates :birthday
  end
 end
